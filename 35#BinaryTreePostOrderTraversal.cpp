252631717a087ef560358e71d0cccbf44d3fada1
PC-2 <PC-2@SD-SURESHV>
2023-08-28 13:49:25 +0530

    Rev[24]

---
+++ 35#BinaryTreePostOrderTraversal.cpp
@@ -0,0 +1,44 @@
+/*
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+ 
+Constraints:
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> ans;
+vector<int> postorderTraversal(TreeNode* root) {
+    if(root == NULL) return ans;
+    postorderTraversal(root->left);
+    postorderTraversal(root->right);
+    ans.push_back(root->val);
+    return ans;
+}
+
+int main(){
+    return 0;
+}
