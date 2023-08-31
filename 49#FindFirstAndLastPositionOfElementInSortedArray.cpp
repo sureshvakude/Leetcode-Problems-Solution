6c85ac01243386f6c5ac56ff565f6b8b57691c76
suresh <sureshvakude8906@gmail.com>
2023-09-01 00:29:19 +0530

    Rev[28]

---
+++ 49#FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,47 @@
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting 
+and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+
+Example 2:
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+
+Example 3:
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+Constraints:
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> SearchRange(vector<int>& nums, int target) {
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> nums;
+    for(int i=0;i<n;i++){
+        int tp;
+        cin>>tp;
+        nums.push_back(tp);
+    }
+
+    int target;
+    cin>>target;
+    vector<int> ans = SearchRange(nums,target);
+}
