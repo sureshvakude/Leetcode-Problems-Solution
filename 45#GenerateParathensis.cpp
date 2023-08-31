f206ba95a0b6d37f7deae473f92520f1a789f353
PC-2 <PC-2@SD-SURESHV>
2023-08-31 17:04:36 +0530

    Rev[27]

--- 45#GenerateParathensis.cpp
+++ 45#GenerateParathensis.cpp
@@ -16,8 +16,30 @@ Constraints:
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(int open, int close, vector<string> &ans, string s){
+    if(open==0 && close==0){
+        ans.push_back(s);
+        return;
+    }
+    if(open>0){
+        s.push_back('(');
+        solve(open-1, close, ans, s);
+        s.pop_back();
+    }
+    if(close>open){
+        s.push_back(')');
+        solve(open, close-1, ans, s);
+        s.pop_back();
+    }
+}
+
 vector<string> GenerateParenthesis(int n) {
-        
+    vector<string> ans;
+    int open=n;
+    int close=n;
+    string s="";
+    solve(open, close, ans, s);
+    return ans;
 }
 
 int main(){
---
+++ 46#DivideTwoInteger.cpp
@@ -0,0 +1,54 @@
+/*
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be 
+truncated to 8, and -2.7335 would be truncated to -2.
+Return the quotient after dividing dividend by divisor.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+
+Example 1:
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+Example 2:
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = -2.33333.. which is truncated to -2.
+ 
+Constraints:
+-231 <= dividend, divisor <= 231 - 1
+divisor != 0
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int divide(int dividend, int divisor) {
+    if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+    if(dividend==INT_MIN && divisor==1) return INT_MIN;
+
+    long int dd = abs(dividend), dv = abs(divisor);
+        
+    int res=0;
+    while(dv<=dd) {
+        long int mul=dv, tmp=1;
+        while(mul<=dd-mul) {
+            mul+=mul;
+            tmp+=tmp;
+        }
+        res+=tmp;
+        dd-=mul;
+    }
+        
+    if((dividend<0&&divisor>0) || (dividend>0&&divisor<0)) return -res;
+    return res; 
+}
+
+int main(){
+    int dividend,divisor;
+    cin>>dividend;
+    cin>>divisor;
+
+    cout<<divide(dividend,divisor);
+}
